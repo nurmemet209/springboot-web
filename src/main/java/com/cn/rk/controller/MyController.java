1b601cfb47dd2fb7205b92ba07cf7bb31889981d
2335515050@qq.com <nurmemet09>
2016-12-22 23:26:04 +0800

    springboot基本知识之多数据源配置

--- src/main/java/com/cn/rk/controller/MyController.java
+++ src/main/java/com/cn/rk/controller/MyController.java
@@ -4,20 +4,47 @@ package com.cn.rk.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 12/18/2016.
+ * 返回json数据
  */
-@RestController
+@Controller
 public class MyController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/test")
-    public String test(){
+    @ResponseBody
+    public Test test(){
         logger.info("/test");
-        return "testdfdfdsfse";
+        return new Test();
+    }
+
+
+    private static class Test {
+        String userName="nurmemet";
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        int id=23;
     }
 }
